c2b6ab07ddafff4fc1c365efd2f8395a8396ccfc
Viber <mouadmoncif1@gmail.com>
2025-08-01 02:28:15 +0100

    setup the ci pipeline and added dbt_utils dependency and a surrogate key to staging_fact_table

--- dags/dbt/models/staging/stg_fact_table.sql
+++ dags/dbt/models/staging/stg_fact_table.sql
@@ -1,4 +1,5 @@
-select vendorid as vendor_id,
+select 
+vendorid as vendor_id,
 to_char(readable_tpep_pickup_datetime, 'HH24') || to_char(readable_tpep_pickup_datetime, 'mi') || to_char(readable_tpep_pickup_datetime, 'ss') as pickup_time_id,
 to_char(readable_tpep_dropoff_datetime, 'HH24') || to_char(readable_tpep_dropoff_datetime, 'mi') || to_char(readable_tpep_dropoff_datetime, 'ss') as dropoff_time_id,
 date_part(epoch_microsecond,(to_char(readable_tpep_pickup_datetime,'yyyy')|| '-' || to_char(readable_tpep_pickup_datetime,'mm')|| '-' || to_char(readable_tpep_pickup_datetime,'dd'))::timestamp_ntz)::varchar as date_id,
@@ -16,6 +17,7 @@ abs(tip_amount) as tip_amount,
 abs(tolls_amount) as tolls_amount,
 abs(improvement_surcharge) as improvement_surcharge,
 abs(total_amount) as total_amount,
+{{dbt_utils.generate_surrogate_key(['pickup_time_id', 'dropoff_time_id','date_id','pulocation_id','dolocation_id']) }} as trip_id,
 abs(congestion_surcharge) as congestion_surcharge
 from {{source('taxi','yellow_taxi_trip')}}
 where to_char(readable_tpep_pickup_datetime,'yyyy') = '2024' and to_char(readable_tpep_pickup_datetime,'mm')::int between 1 and 12
